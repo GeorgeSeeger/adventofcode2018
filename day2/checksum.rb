1127d9e44427bbcbd7c0aae05b4db5f44b735c3d
George Seeger <georgeseeger@hotmail.com>
2018-12-02 12:31:25 +0000

    Day 2 Part 2: reminder not to assume about ruby methods

--- day2/checksum.rb
+++ day2/checksum.rb
@@ -8,3 +8,16 @@ twos = input.count{|id| hasRepeats?(id, 2)}
 threes = input.count{|id| hasRepeats?(id, 3)}
 
 puts twos * threes
+
+## part 2
+input.each do |id|
+  near_match = input.select{ |id2|
+    (id2.chars.select.with_index{|c2, i| c2 != id.chars[i] }).count == 1
+  }.first
+
+  if (near_match != nil)
+    puts id.chars.select.with_index{|c, i| near_match[i] == c }.join
+    break
+  end
+
+end
