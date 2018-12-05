70adaef96b24fcfd815cf7191fea699f198936d4
George Seeger <george@polylytics.com>
2018-12-05 11:20:12 +0000

    brüüt force methods

--- day5/polymer.rb
+++ day5/polymer.rb
@@ -1,15 +1,24 @@
 input = File.readlines("day5/input.txt")[0].chomp()
 
-len = nil
-until (len == input.length) do
-    len = input.length
-    (0...len-1).each do |i|
-        a, b = input[i], input[i+1]
-        if (a.ord + 32 == b.ord || a.ord == b.ord + 32) 
-            input[i], input[i+1] = "|", "|"
+def react_polymer(str)
+    len = nil
+    until (len == str.length) do
+        len = str.length
+        (0...len-1).each do |i|
+            a, b = str[i], str[i+1]
+            if (a.ord + 32 == b.ord || a.ord == b.ord + 32) 
+                str[i], str[i+1] = "|", "|"
+            end
         end
+        str = str.chars.reject{|c| c == "|"}.join
     end
-    input = input.chars.reject{|c| c == "|"}.join
+    str
 end
 
-puts input.length
+# part 1
+puts react_polymer(input).length
+
+# part 2
+puts (65..90).map{ |i|
+    react_polymer(input.gsub(i.chr, "").gsub((i + 32).chr, "")).length
+}.min
