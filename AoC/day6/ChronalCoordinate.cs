e56bb8eb197f873b1412164b44a1e864e386d5f0
George Seeger <george@polylytics.com>
2018-12-06 16:29:08 +0000

    aaaaaaaaaa

--- AoC/day6/ChronalCoordinate.cs
+++ AoC/day6/ChronalCoordinate.cs
@@ -5,11 +5,60 @@ namespace AoC {
     using System.IO;
 
     public class ChronalCoordinates {
+        public Coord origin { get; private set; }
+        public int Id { get; }
+
+        private static int id = 0;
+
+        public ChronalCoordinates(Coord origin) {
+            this.origin = origin;
+            this.Id = id++;
+        }
+
+        private static int[][] GenerateBoard(IEnumerable<ChronalCoordinates> groups, int offset = 0) {
+            var xMax = groups.Select(c => c.origin.X).Max();
+            var yMax = groups.Select(c => c.origin.Y).Max();
+            var board = new int[yMax + 2 * offset][];
+            for (var i = 0; i < xMax; i++) {
+                board[i] = new int[xMax + 2 * offset];
+            }
+            foreach (var group in groups) {
+                board[group.origin.Y + offset][group.origin.X + offset] = group.Id;
+            }
+            
+            for (var i = 0; i < board.Length; i++) {
+                for (var j = 0; j < board[0].Length; j++) {
+                    MarkWithNearestId(board, i, j);
+                }
+            }
+
+            return board;
+        }
+        private static void MarkWithNearestId(int[][] board, int i, int j) {
+            var here = new Coord(j, i);
+            var spread = new[] {here};
+            while (spread.All(c => GetValueAt(board, c) == 0)) {
+                spread = spread.SelectMany(c => UnitDirections.Select(c1 => c1 + c)).Distinct().ToArray();
+                var nonEmpty = spread.Where(c => GetValueAt(board, c) != 0);
+                if (nonEmpty.Any()) {
+                    
+                }
+            }
+        }
+
+
         private static IEnumerable<Coord> UnitDirections = new[] { new Coord(0, 1), new Coord(1, 0), new Coord(0, -1), new Coord(-1, 0) };
 
+        private static int GetValueAt(int[][] board, Coord c) {
+            var yMax = board.Length;
+            var xMax = board[0].Length;
+
+            if (c.Y < 0 || c.Y >= yMax || c.X < 0 || c.X >= xMax) return 0;
+            else return board[c.Y][c.X];
+        }
         public static void Solve() {
             var input = File.ReadAllLines("AoC/Day6/input.txt");
-            var groups = input.Select(s => s.Split(", ").Select(int.Parse).ToArray()).Select(a => new Coord(a[0], a[1]));
+            var groups = input.Select(s => s.Split(", ").Select(int.Parse).ToArray()).Select(a => new ChronalCoordinates(new Coord(a[0], a[1])));
         }
     }
 
